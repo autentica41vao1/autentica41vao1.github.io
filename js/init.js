3530633acdbf21325a93d9e7804dc5526784d8a9
autentica41vao <autentica41vao@gmail.com>
2023-03-16 23:49:44 -0600

    cambios

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyCK4wa72A-8kopsVOW64w1j5_LHbkjWn2w",
+  authDomain: "autentica41vao2.firebaseapp.com",
+  projectId: "autentica41vao2",
+  storageBucket: "autentica41vao2.appspot.com",
+  messagingSenderId: "647738204013",
+  appId: "1:647738204013:web:1ffdc60a9b74f3f05c4464",
+  measurementId: "G-WYMCB70Y7L"
+    });
